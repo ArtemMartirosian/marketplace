472cc69c4a9002acccc6e3fec1473a13769eb0f2
ArtemMartirosian <artem.martirosian2001@gmail.com>
2025-05-19 13:38:19 +0400

    fixes

--- src/features/header/constants/menu.constants.ts
+++ src/features/header/constants/menu.constants.ts
@@ -1,5 +1,5 @@
 export const MENU_CONSTANT = [
     {id : 1, name : "проекты", link : "/"},
-    {id : 2, name : "о платформе inwis", link : "/"},
+    {id : 2, name : "о платформе inwis", link : "/about"},
     {id : 3, name : "раскрытие информации", link : "/"},
 ]
