c54f589cdd975d94e5a22066afc9b423af69e3ac
Dev <zad07an@gmail.com>
2025-05-27 17:46:15 +0400

    added input range

---
+++ src/Components/ui/dual-range-slider.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "@/lib/utils";
+
+interface DualRangeSliderProps
+  extends React.ComponentProps<typeof SliderPrimitive.Root> {
+  labelPosition?: "top" | "bottom";
+  label?: (value: number | undefined) => React.ReactNode;
+}
+
+const DualRangeSlider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  DualRangeSliderProps
+>(({ className, label, labelPosition = "top", ...props }, ref) => {
+  const initialValue = Array.isArray(props.value)
+    ? props.value
+    : [props.min, props.max];
+
+  return (
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative flex w-full touch-none select-none items-center",
+        className
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-px w-full grow overflow-hidden rounded-full bg-primary-blue">
+        <SliderPrimitive.Range className="absolute h-full bg-primary-blue" />
+      </SliderPrimitive.Track>
+      {initialValue.map((value, index) => (
+        <React.Fragment key={index}>
+          <SliderPrimitive.Thumb
+            className="relative block h-3 w-3 rounded-full bg-primary-blue disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none cursor-pointer transition-transform duration-300"
+            style={{ width: "12px", height: "12px" }}
+          >
+            {label && (
+              <span
+                className={cn(
+                  "absolute flex w-full justify-center",
+                  labelPosition === "top" && "-top-7",
+                  labelPosition === "bottom" && "top-4"
+                )}
+              >
+                {label(value)}
+              </span>
+            )}
+          </SliderPrimitive.Thumb>
+        </React.Fragment>
+      ))}
+    </SliderPrimitive.Root>
+  );
+});
+DualRangeSlider.displayName = "DualRangeSlider";
+
+export { DualRangeSlider };
